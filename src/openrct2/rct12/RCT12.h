521cee6c448c887192f14413b01601180241c25f
Gymnasiast <m.o.steenbeek@gmail.com>
2018-10-31 20:49:24 +0100

    Split TileElement and RCT12TileElement

--- src/openrct2/rct12/RCT12.h
+++ src/openrct2/rct12/RCT12.h
@@ -12,6 +12,7 @@
 // Structures shared between both RCT1 and RCT2.
 
 #include "../common.h"
+#include "../world/Location.hpp"
 
 #define RCT12_MAX_RIDES_IN_PARK 255
 #define RCT12_MAX_AWARDS 4
@@ -76,5 +77,417 @@ struct rct12_peep_spawn
     uint8_t direction;
 };
 assert_struct_size(rct12_peep_spawn, 6);
+
+enum class RCT12TileElementType : uint8_t
+{
+    Surface = (0 << 2),
+    Path = (1 << 2),
+    Track = (2 << 2),
+    SmallScenery = (3 << 2),
+    Entrance = (4 << 2),
+    Wall = (5 << 2),
+    LargeScenery = (6 << 2),
+    Banner = (7 << 2),
+    Corrupt = (8 << 2),
+};
+
+struct RCT12SurfaceElement;
+struct RCT12PathElement;
+struct RCT12TrackElement;
+struct RCT12SmallSceneryElement;
+struct RCT12LargeSceneryElement;
+struct RCT12WallElement;
+struct RCT12EntranceElement;
+struct RCT12BannerElement;
+struct RCT12CorruptElement;
+
+struct RCT12TileElementBase
+{
+    uint8_t type;             // 0
+    uint8_t flags;            // 1
+    uint8_t base_height;      // 2
+    uint8_t clearance_height; // 3
+
+    uint8_t GetType() const;
+    void SetType(uint8_t newType);
+    uint8_t GetDirection() const;
+    void SetDirection(uint8_t direction);
+    uint8_t GetDirectionWithOffset(uint8_t offset) const;
+    bool IsLastForTile() const;
+    bool IsGhost() const;
+    void Remove();
+};
+
+/**
+ * Map element structure
+ * size: 0x08
+ */
+struct RCT12TileElement : public RCT12TileElementBase
+{
+    uint8_t pad_04[4];
+
+    template<typename TType, RCT12TileElementType TClass> TType* as() const
+    {
+        return (RCT12TileElementType)GetType() == TClass ? (TType*)this : nullptr;
+    }
+
+public:
+    RCT12SurfaceElement* AsSurface() const
+    {
+        return as<RCT12SurfaceElement, RCT12TileElementType::Surface>();
+    }
+    RCT12PathElement* AsPath() const
+    {
+        return as<RCT12PathElement, RCT12TileElementType::Path>();
+    }
+    RCT12TrackElement* AsTrack() const
+    {
+        return as<RCT12TrackElement, RCT12TileElementType::Track>();
+    }
+    RCT12SmallSceneryElement* AsSmallScenery() const
+    {
+        return as<RCT12SmallSceneryElement, RCT12TileElementType::SmallScenery>();
+    }
+    RCT12LargeSceneryElement* AsLargeScenery() const
+    {
+        return as<RCT12LargeSceneryElement, RCT12TileElementType::LargeScenery>();
+    }
+    RCT12WallElement* AsWall() const
+    {
+        return as<RCT12WallElement, RCT12TileElementType::Wall>();
+    }
+    RCT12EntranceElement* AsEntrance() const
+    {
+        return as<RCT12EntranceElement, RCT12TileElementType::Entrance>();
+    }
+    RCT12BannerElement* AsBanner() const
+    {
+        return as<RCT12BannerElement, RCT12TileElementType::Banner>();
+    }
+    RCT12CorruptElement* AsCorrupt() const
+    {
+        return as<RCT12CorruptElement, RCT12TileElementType::Corrupt>();
+    }
+
+    void ClearAs(uint8_t newType);
+};
+assert_struct_size(RCT12TileElement, 8);
+
+struct RCT12SurfaceElement : RCT12TileElementBase
+{
+private:
+    uint8_t slope;        // 4 0xE0 Edge Style, 0x1F Slope
+    uint8_t terrain;      // 5 0xE0 Terrain Style, 0x1F Water height
+    uint8_t grass_length; // 6
+    uint8_t ownership;    // 7
+public:
+    uint8_t GetSlope() const;
+    void SetSlope(uint8_t newSlope);
+
+    uint32_t GetSurfaceStyle() const;
+    void SetSurfaceStyle(uint32_t newStyle);
+    uint32_t GetEdgeStyle() const;
+    void SetEdgeStyle(uint32_t newStyle);
+
+    uint8_t GetGrassLength() const;
+    void SetGrassLength(uint8_t newLength);
+    void SetGrassLengthAndInvalidate(uint8_t newLength, CoordsXY coords);
+    void UpdateGrassLength(CoordsXY coords);
+
+    uint8_t GetOwnership() const;
+    void SetOwnership(uint8_t newOwnership);
+
+    uint32_t GetWaterHeight() const;
+    void SetWaterHeight(uint32_t newWaterHeight);
+
+    uint8_t GetParkFences() const;
+    void SetParkFences(uint8_t newParkFences);
+
+    bool HasTrackThatNeedsWater() const;
+    void SetHasTrackThatNeedsWater(bool on);
+};
+assert_struct_size(RCT12SurfaceElement, 8);
+
+struct RCT12PathElement : RCT12TileElementBase
+{
+private:
+    uint8_t entryIndex; // 4, 0xF0 Path type, 0x08 Ride sign, 0x04 Set when path is sloped, 0x03 Rotation
+    uint8_t additions;  // 5, 0bGSSSAAAA: G = Ghost, S = station index, A = addition (0 means no addition)
+    uint8_t edges;      // 6
+    union
+    {
+        uint8_t additionStatus; // 7
+        uint8_t rideIndex;
+    };
+
+public:
+    uint8_t GetEntryIndex() const;
+    void SetEntryIndex(uint8_t newIndex);
+
+    uint8_t GetQueueBannerDirection() const;
+    void SetQueueBannerDirection(uint8_t direction);
+
+    bool IsSloped() const;
+    void SetSloped(bool isSloped);
+
+    uint8_t GetSlopeDirection() const;
+    void SetSlopeDirection(uint8_t newSlope);
+
+    uint8_t GetRideIndex() const;
+    void SetRideIndex(uint8_t newRideIndex);
+
+    uint8_t GetStationIndex() const;
+    void SetStationIndex(uint8_t newStationIndex);
+
+    bool IsWide() const;
+    void SetWide(bool isWide);
+
+    bool IsQueue() const;
+    void SetIsQueue(bool isQueue);
+    bool HasQueueBanner() const;
+    void SetHasQueueBanner(bool hasQueueBanner);
+
+    uint8_t GetEdges() const;
+    void SetEdges(uint8_t newEdges);
+    uint8_t GetCorners() const;
+    void SetCorners(uint8_t newCorners);
+    uint8_t GetEdgesAndCorners() const;
+    void SetEdgesAndCorners(uint8_t newEdgesAndCorners);
+
+    bool HasAddition() const;
+    uint8_t GetAddition() const;
+    uint8_t GetAdditionEntryIndex() const;
+    void SetAddition(uint8_t newAddition);
+
+    bool AdditionIsGhost() const;
+    void SetAdditionIsGhost(bool isGhost);
+
+    uint8_t GetAdditionStatus() const;
+    void SetAdditionStatus(uint8_t newStatus);
+
+    uint8_t GetRCT1PathType() const;
+};
+assert_struct_size(RCT12PathElement, 8);
+
+struct RCT12TrackElement : RCT12TileElementBase
+{
+    uint8_t trackType; // 4
+    union
+    {
+        struct
+        {
+            // The lower 4 bits are the track sequence.
+            // The upper 4 bits are either station bits or on-ride photo bits.
+            //
+            // Station bits:
+            // - Bit 8 marks green light
+            // - Bit 5-7 are station index.
+            //
+            // On-ride photo bits:
+            // - Bits 7 and 8 are never set
+            // - Bits 5 and 6 are set when a vehicle triggers the on-ride photo and act like a countdown from 3.
+            // - If any of the bits 5-8 are set, the game counts it as a photo being taken.
+            uint8_t sequence; // 5.
+            uint8_t colour;   // 6
+        };
+        uint16_t mazeEntry; // 5
+    };
+    uint8_t rideIndex; // 7
+
+public:
+    uint8_t GetTrackType() const;
+    void SetTrackType(uint8_t newEntryIndex);
+
+    uint8_t GetSequenceIndex() const;
+    void SetSequenceIndex(uint8_t newSequenceIndex);
+
+    uint8_t GetRideIndex() const;
+    void SetRideIndex(uint8_t newRideIndex);
+
+    uint8_t GetColourScheme() const;
+    void SetColourScheme(uint8_t newColourScheme);
+
+    uint8_t GetStationIndex() const;
+    void SetStationIndex(uint8_t newStationIndex);
+
+    bool HasChain() const;
+    void SetHasChain(bool on);
+
+    bool HasCableLift() const;
+    void SetHasCableLift(bool on);
+
+    bool IsInverted() const;
+    void SetInverted(bool inverted);
+
+    uint8_t GetBrakeBoosterSpeed() const;
+    void SetBrakeBoosterSpeed(uint8_t speed);
+
+    uint8_t HasGreenLight() const;
+    void SetHasGreenLight(uint8_t greenLight);
+
+    uint8_t GetSeatRotation() const;
+    void SetSeatRotation(uint8_t newSeatRotation);
+
+    uint16_t GetMazeEntry() const;
+    void SetMazeEntry(uint16_t newMazeEntry);
+    void MazeEntryAdd(uint16_t addVal);
+    void MazeEntrySubtract(uint16_t subVal);
+
+    bool IsTakingPhoto() const;
+    void SetPhotoTimeout();
+    void DecrementPhotoTimeout();
+
+    bool IsHighlighted() const;
+    void SetHighlight(bool on);
+
+    // Used in RCT1, will be reintroduced at some point.
+    // (See https://github.com/OpenRCT2/OpenRCT2/issues/7059)
+    uint8_t GetDoorAState() const;
+    uint8_t GetDoorBState() const;
+};
+assert_struct_size(RCT12TrackElement, 8);
+
+struct RCT12SmallSceneryElement : RCT12TileElementBase
+{
+private:
+    uint8_t entryIndex; // 4
+    uint8_t age;        // 5
+    uint8_t colour_1;   // 6
+    uint8_t colour_2;   // 7
+public:
+    uint8_t GetEntryIndex() const;
+    void SetEntryIndex(uint8_t newIndex);
+    uint8_t GetAge() const;
+    void SetAge(uint8_t newAge);
+    void IncreaseAge(int32_t x, int32_t y);
+    uint8_t GetSceneryQuadrant() const;
+    void SetSceneryQuadrant(uint8_t newQuadrant);
+    colour_t GetPrimaryColour() const;
+    void SetPrimaryColour(colour_t colour);
+    colour_t GetSecondaryColour() const;
+    void SetSecondaryColour(colour_t colour);
+    bool NeedsSupports() const;
+    void SetNeedsSupports();
+};
+assert_struct_size(RCT12SmallSceneryElement, 8);
+
+struct RCT12LargeSceneryElement : RCT12TileElementBase
+{
+private:
+    uint16_t entryIndex; // 4
+    uint8_t colour[2];   // 6
+public:
+    uint32_t GetEntryIndex() const;
+    void SetEntryIndex(uint32_t newIndex);
+
+    uint16_t GetSequenceIndex() const;
+    void SetSequenceIndex(uint16_t newIndex);
+
+    colour_t GetPrimaryColour() const;
+    void SetPrimaryColour(colour_t colour);
+    colour_t GetSecondaryColour() const;
+    void SetSecondaryColour(colour_t colour);
+
+    BannerIndex GetBannerIndex() const;
+    void SetBannerIndex(BannerIndex newIndex);
+};
+assert_struct_size(RCT12LargeSceneryElement, 8);
+
+struct RCT12WallElement : RCT12TileElementBase
+{
+private:
+    uint8_t entryIndex; // 4
+    union
+    {
+        uint8_t colour_3;         // 5
+        BannerIndex banner_index; // 5
+    };
+    uint8_t colour_1;  // 6 0b_2221_1111 2 = colour_2 (uses flags for rest of colour2), 1 = colour_1
+    uint8_t animation; // 7 0b_dfff_ft00 d = direction, f = frame num, t = across track flag (not used)
+
+public:
+    uint8_t GetEntryIndex() const;
+    void SetEntryIndex(uint8_t newIndex);
+
+    uint8_t GetSlope() const;
+    void SetSlope(uint8_t newslope);
+
+    colour_t GetPrimaryColour() const;
+    void SetPrimaryColour(colour_t newColour);
+    colour_t GetSecondaryColour() const;
+    void SetSecondaryColour(colour_t newColour);
+    colour_t GetTertiaryColour() const;
+    void SetTertiaryColour(colour_t newColour);
+
+    uint8_t GetAnimationFrame() const;
+    void SetAnimationFrame(uint8_t frameNum);
+
+    BannerIndex GetBannerIndex() const;
+    void SetBannerIndex(BannerIndex newIndex);
+
+    bool IsAcrossTrack() const;
+    void SetAcrossTrack(bool acrossTrack);
+    bool AnimationIsBackwards() const;
+    void SetAnimationIsBackwards(bool isBackwards);
+
+    int32_t GetRCT1WallType(int32_t edge) const;
+    colour_t GetRCT1WallColour() const;
+};
+assert_struct_size(RCT12WallElement, 8);
+
+struct RCT12EntranceElement : RCT12TileElementBase
+{
+private:
+    uint8_t entranceType; // 4
+    uint8_t index;        // 5. 0bUSSS????, S = station index.
+    uint8_t pathType;     // 6
+    uint8_t rideIndex;    // 7
+
+public:
+    uint8_t GetEntranceType() const;
+    void SetEntranceType(uint8_t newType);
+
+    uint8_t GetRideIndex() const;
+    void SetRideIndex(uint8_t newRideIndex);
+
+    uint8_t GetStationIndex() const;
+    void SetStationIndex(uint8_t stationIndex);
+
+    uint8_t GetSequenceIndex() const;
+    void SetSequenceIndex(uint8_t newSequenceIndex);
+
+    uint8_t GetPathType() const;
+    void SetPathType(uint8_t newPathType);
+};
+assert_struct_size(RCT12EntranceElement, 8);
+
+struct RCT12BannerElement : RCT12TileElementBase
+{
+private:
+    BannerIndex index; // 4
+    uint8_t position;  // 5
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wunused-private-field"
+    uint8_t flags;  // 6
+    uint8_t unused; // 7
+#pragma clang diagnostic pop
+public:
+    BannerIndex GetIndex() const;
+    void SetIndex(BannerIndex newIndex);
+
+    uint8_t GetPosition() const;
+    void SetPosition(uint8_t newPosition);
+
+    uint8_t GetAllowedEdges() const;
+    void SetAllowedEdges(uint8_t newEdges);
+    void ResetAllowedEdges();
+};
+assert_struct_size(RCT12BannerElement, 8);
+
+struct RCT12CorruptElement : RCT12TileElementBase
+{
+    uint8_t pad[4];
+};
+assert_struct_size(RCT12CorruptElement, 8);
 
 #pragma pack(pop)
--- src/openrct2/rct2/RCT2.h
+++ src/openrct2/rct2/RCT2.h
@@ -7,14 +7,17 @@
  * OpenRCT2 is licensed under the GNU General Public License version 3.
  *****************************************************************************/
 
-#ifndef _RCT2_H_
-#define _RCT2_H_
+#pragma once
 
 #include "../common.h"
+#include "../object/Object.h"
 #include "../rct12/RCT12.h"
 #include "../ride/RideRatings.h"
 #include "../ride/Vehicle.h"
+#include "../world/Banner.h"
 #include "../world/Location.hpp"
+#include "../world/MapAnimation.h"
+#include "../world/Sprite.h"
 
 #define RCT2_MAX_STAFF 200
 #define RCT2_MAX_BANNERS_IN_PARK 250
@@ -32,6 +35,8 @@
 #define RCT2_MAX_RESEARCHED_RIDE_ENTRY_QUADS 8 // With 32 bits per uint32_t, this means there is room for 256 entries.
 #define RCT2_MAX_RESEARCHED_SCENERY_ITEM_QUADS 56
 #define RCT2_MAX_RESEARCHED_SCENERY_ITEMS (RCT2_MAX_RESEARCHED_SCENERY_ITEM_QUADS * 32) // There are 32 bits per quad.
+#define RCT2_MAX_RESEARCH_ITEMS 500
+#define RCT2_RIDE_MEASUREMENT_MAX_ITEMS 4800
 
 struct rct2_install_info
 {
@@ -45,6 +50,48 @@ struct rct2_install_info
 };
 
 #pragma pack(push, 1)
+
+/**
+ * Ride measurement structure.
+ * size: 0x04B0C
+ */
+struct rct2_ride_measurement
+{
+    uint8_t ride_index;                                // 0x0000
+    uint8_t flags;                                     // 0x0001
+    uint32_t last_use_tick;                            // 0x0002
+    uint16_t num_items;                                // 0x0006
+    uint16_t current_item;                             // 0x0008
+    uint8_t vehicle_index;                             // 0x000A
+    uint8_t current_station;                           // 0x000B
+    int8_t vertical[RCT2_RIDE_MEASUREMENT_MAX_ITEMS];  // 0x000C
+    int8_t lateral[RCT2_RIDE_MEASUREMENT_MAX_ITEMS];   // 0x12CC
+    uint8_t velocity[RCT2_RIDE_MEASUREMENT_MAX_ITEMS]; // 0x258C
+    uint8_t altitude[RCT2_RIDE_MEASUREMENT_MAX_ITEMS]; // 0x384C
+};
+assert_struct_size(rct2_ride_measurement, 0x4b0c);
+
+struct rct2_research_item
+{
+    // Bit 16 (0: scenery entry, 1: ride entry)
+    union
+    {
+        int32_t rawValue;
+        struct
+        {
+            uint8_t entryIndex;
+            uint8_t baseRideType;
+            uint8_t type; // 0: scenery entry, 1: ride entry
+            uint8_t flags;
+        };
+    };
+    uint8_t category;
+
+    bool IsInventedEndMarker() const;
+    bool IsRandomEndMarker() const;
+    bool IsUninventedEndMarker() const;
+};
+assert_struct_size(rct2_research_item, 5);
 
 /**
  * Ride structure.
@@ -302,6 +349,251 @@ struct rct_scores_entry
 };
 assert_struct_size(rct_scores_entry, 0x02B0);
 
-#pragma pack(pop)
+/**
+ * SV6/SC6 header chunk
+ * size: 0x20
+ */
+struct rct_s6_header
+{
+    uint8_t type;                // 0x00
+    uint8_t classic_flag;        // 0x01
+    uint16_t num_packed_objects; // 0x02
+    uint32_t version;            // 0x04
+    uint32_t magic_number;       // 0x08
+    uint8_t pad_0C[0x14];
+};
+assert_struct_size(rct_s6_header, 0x20);
 
-#endif
+/**
+ * SC6 information chunk
+ * size: 0x198
+ */
+struct rct_s6_info
+{
+    uint8_t editor_step;
+    uint8_t category;        // 0x01
+    uint8_t objective_type;  // 0x02
+    uint8_t objective_arg_1; // 0x03
+    int32_t objective_arg_2; // 0x04
+    int16_t objective_arg_3; // 0x08
+    uint8_t pad_00A[0x3E];
+    char name[64];          // 0x48
+    char details[256];      // 0x88
+    rct_object_entry entry; // 0x188
+};
+assert_struct_size(rct_s6_info, 0x198);
+
+struct rct_s6_data
+{
+    // SC6[0]
+    rct_s6_header header;
+
+    // SC6[1]
+    rct_s6_info info;
+
+    // SC6[2]
+    // packed objects
+
+    // SC6[3]
+    rct_object_entry objects[OBJECT_ENTRY_COUNT];
+
+    // SC6[4]
+    uint16_t elapsed_months;
+    uint16_t current_day;
+    uint32_t scenario_ticks;
+    uint32_t scenario_srand_0;
+    uint32_t scenario_srand_1;
+
+    // SC6[5]
+    TileElement tile_elements[RCT2_MAX_TILE_ELEMENTS];
+
+    // SC6[6]
+    uint32_t next_free_tile_element_pointer_index;
+    rct_sprite sprites[RCT2_MAX_SPRITES];
+    uint16_t sprite_lists_head[6];
+    uint16_t sprite_lists_count[6];
+    rct_string_id park_name;
+    uint8_t pad_013573D6[2];
+    uint32_t park_name_args;
+    money32 initial_cash;
+    money32 current_loan;
+    uint32_t park_flags;
+    money16 park_entrance_fee;
+    uint16_t rct1_park_entrance_x;
+    uint16_t rct1_park_entrance_y;
+    uint8_t pad_013573EE[2];
+    uint8_t rct1_park_entrance_z;
+    uint8_t pad_013573F1;
+    rct12_peep_spawn peep_spawns[RCT12_MAX_PEEP_SPAWNS];
+    uint8_t guest_count_change_modifier;
+    uint8_t current_research_level;
+    uint8_t pad_01357400[4];
+    uint32_t researched_ride_types[RCT2_MAX_RESEARCHED_RIDE_TYPE_QUADS];
+    uint32_t researched_ride_entries[RCT2_MAX_RESEARCHED_RIDE_ENTRY_QUADS];
+    uint32_t researched_track_types_a[128];
+    uint32_t researched_track_types_b[128];
+
+    // SC6[7]
+    uint16_t guests_in_park;
+    uint16_t guests_heading_for_park;
+
+    // Ignored in scenario
+    money32 expenditure_table[RCT12_EXPENDITURE_TABLE_MONTH_COUNT][RCT12_EXPENDITURE_TYPE_COUNT];
+
+    // SC6[8]
+    uint16_t last_guests_in_park;
+    uint8_t pad_01357BCA[3];
+    uint8_t handyman_colour;
+    uint8_t mechanic_colour;
+    uint8_t security_colour;
+
+    // Ignored in scenario
+    uint32_t researched_scenery_items[RCT2_MAX_RESEARCHED_SCENERY_ITEM_QUADS];
+
+    // SC6[9]
+    uint16_t park_rating;
+
+    // Ignored in scenario
+    uint8_t park_rating_history[32];
+    uint8_t guests_in_park_history[32];
+
+    // SC6[10]
+    uint8_t active_research_types;
+    uint8_t research_progress_stage;
+    uint32_t last_researched_item_subject;
+    uint8_t pad_01357CF8[1000];
+    uint32_t next_research_item;
+    uint16_t research_progress;
+    uint8_t next_research_category;
+    uint8_t next_research_expected_day;
+    uint8_t next_research_expected_month;
+    uint8_t guest_initial_happiness;
+    uint16_t park_size;
+    uint16_t guest_generation_probability;
+    uint16_t total_ride_value_for_money;
+    money32 maximum_loan;
+    money16 guest_initial_cash;
+    uint8_t guest_initial_hunger;
+    uint8_t guest_initial_thirst;
+    uint8_t objective_type;
+    uint8_t objective_year;
+    uint8_t pad_013580FA[2];
+    money32 objective_currency;
+    uint16_t objective_guests;
+    uint8_t campaign_weeks_left[20];
+    uint8_t campaign_ride_index[22];
+
+    // Ignored in scenario
+    money32 balance_history[RCT12_FINANCE_GRAPH_SIZE];
+
+    // SC6[11]
+    money32 current_expenditure;
+    money32 current_profit;
+    uint32_t weekly_profit_average_dividend;
+    uint16_t weekly_profit_average_divisor;
+    uint8_t pad_0135833A[2];
+
+    // Ignored in scenario
+    money32 weekly_profit_history[RCT12_FINANCE_GRAPH_SIZE];
+
+    // SC6[12]
+    money32 park_value;
+
+    // Ignored in scenario
+    money32 park_value_history[RCT12_FINANCE_GRAPH_SIZE];
+
+    // SC6[13]
+    money32 completed_company_value;
+    uint32_t total_admissions;
+    money32 income_from_admissions;
+    money32 company_value;
+    uint8_t peep_warning_throttle[16];
+    rct12_award awards[RCT12_MAX_AWARDS];
+    money16 land_price;
+    money16 construction_rights_price;
+    uint16_t word_01358774;
+    uint8_t pad_01358776[2];
+    uint32_t cd_key;
+    uint8_t pad_0135877C[64];
+    uint32_t game_version_number;
+    money32 completed_company_value_record;
+    uint32_t loan_hash;
+    uint16_t ride_count;
+    uint8_t pad_013587CA[6];
+    money32 historical_profit;
+    uint8_t pad_013587D4[4];
+    char scenario_completed_name[32];
+    money32 cash;
+    uint8_t pad_013587FC[50];
+    uint16_t park_rating_casualty_penalty;
+    uint16_t map_size_units;
+    uint16_t map_size_minus_2;
+    uint16_t map_size;
+    uint16_t map_max_xy;
+    uint32_t same_price_throughout;
+    uint16_t suggested_max_guests;
+    uint16_t park_rating_warning_days;
+    uint8_t last_entrance_style;
+    uint8_t rct1_water_colour;
+    uint8_t pad_01358842[2];
+    rct2_research_item research_items[RCT2_MAX_RESEARCH_ITEMS];
+    uint16_t map_base_z;
+    char scenario_name[64];
+    char scenario_description[256];
+    uint8_t current_interest_rate;
+    uint8_t pad_0135934B;
+    uint32_t same_price_throughout_extended;
+    int16_t park_entrance_x[RCT12_MAX_PARK_ENTRANCES];
+    int16_t park_entrance_y[RCT12_MAX_PARK_ENTRANCES];
+    int16_t park_entrance_z[RCT12_MAX_PARK_ENTRANCES];
+    uint8_t park_entrance_direction[RCT12_MAX_PARK_ENTRANCES];
+    char scenario_filename[256];
+    uint8_t saved_expansion_pack_names[3256];
+    rct_banner banners[RCT2_MAX_BANNERS_IN_PARK];
+    char custom_strings[RCT12_MAX_USER_STRINGS][RCT12_USER_STRING_MAX_LENGTH];
+    uint32_t game_ticks_1;
+    rct2_ride rides[RCT12_MAX_RIDES_IN_PARK];
+    uint16_t saved_age;
+    uint16_t saved_view_x;
+    uint16_t saved_view_y;
+    uint8_t saved_view_zoom;
+    uint8_t saved_view_rotation;
+    rct_map_animation map_animations[RCT2_MAX_ANIMATED_OBJECTS];
+    uint16_t num_map_animations;
+    uint8_t pad_0138B582[2];
+    rct_ride_rating_calc_data ride_ratings_calc_data;
+    uint8_t pad_0138B5D0[60];
+    rct2_ride_measurement ride_measurements[8];
+    uint32_t next_guest_index;
+    uint16_t grass_and_scenery_tilepos;
+    uint32_t patrol_areas[(RCT2_MAX_STAFF + RCT12_STAFF_TYPE_COUNT) * RCT12_PATROL_AREA_SIZE];
+    uint8_t staff_modes[RCT2_MAX_STAFF + RCT12_STAFF_TYPE_COUNT];
+    uint8_t pad_13CA73E;
+    uint8_t pad_13CA73F;
+    uint8_t byte_13CA740;
+    uint8_t pad_13CA741;
+    uint8_t byte_13CA742[4]; // unused
+    uint8_t climate;
+    uint8_t pad_013CA747;
+    uint16_t climate_update_timer;
+    uint8_t current_weather;
+    uint8_t next_weather;
+    uint8_t temperature;
+    uint8_t next_temperature;
+    uint8_t current_weather_effect;
+    uint8_t next_weather_effect;
+    uint8_t current_weather_gloom;
+    uint8_t next_weather_gloom;
+    uint8_t current_rain_level;
+    uint8_t next_rain_level;
+    rct12_news_item news_items[RCT12_MAX_NEWS_ITEMS];
+    char rct1_scenario_name[62];       // Unused in RCT2
+    uint16_t rct1_scenario_slot_index; // Unused in RCT2
+    uint32_t rct1_scenario_flags;      // Unused in RCT2
+    uint16_t wide_path_tile_loop_x;
+    uint16_t wide_path_tile_loop_y;
+    uint8_t pad_13CE778[434];
+};
+assert_struct_size(rct_s6_data, 0x46b44a);
+
+#pragma pack(pop)
